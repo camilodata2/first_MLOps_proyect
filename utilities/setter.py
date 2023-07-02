e2ec50c7ae9d71f41bd5ac5680d8fd1ef7552516
camilo <pgalvis253@gmail.com>
2023-07-01 19:57:50 -0500

    fixing an error in utilities file

--- utilities/setter.py
+++ utilities/setter.py
@@ -4,7 +4,7 @@ from base64 import b64encode
 def archivo():
     key=os.environ.get("SERVICE_ACCOUNT_KEY")
     with open('path.json','w',encoding='utf8') as json_file:
-        json_file.write(b64encode(key.decode()))
+        json_file.write(b64encode(key).decode())
         print(os.path.realpath('path.json'))
         if __name__== '__main__':
             archivo()
